e0ca89359cde871e0aea573b9dd7ffe6a515d4d9
Helen Medeiros <116672671+byhelenmedeiros@users.noreply.github.com>
2023-05-09 01:05:35 +0100

    corrigir js

--- script.js
+++ script.js
@@ -1,20 +1,3 @@
-$(document).ready(function() {
-    // Seleciona o formulário e adiciona um listener para o evento submit
-    $('form').submit(function(event) {
-        event.preventDefault(); // Impede o formulário de ser enviado
-
-        // Incrementa o valor do progresso em 25%
-        $('.progress-bar').css('width', '+=25%').attr('aria-valuenow', parseInt($('.progress-bar').attr('aria-valuenow')) + 25);
-
-        // Se a barra de progresso atingir 100%, exibe uma mensagem de conclusão
-        if ($('.progress-bar').attr('aria-valuenow') == 100) {
-            $('.progress-bar').addClass('bg-success');
-            $('.text-progress').text('Ficha de anamnese concluída!');
-            $('form :input').prop('disabled', true); // Desabilita todos os campos do formulário
-            $('form :submit').prop('disabled', true).removeClass('btn-primary').addClass('btn-secondary'); // Desabilita o botão de envio do formulário
-        }
-    });
-});
 
 
 function proximaPagina() {
@@ -23,18 +6,6 @@ function proximaPagina() {
 
 const progressBar = document.querySelector('.progress-bar');
 const nextPageBtn = document.querySelector('#btn btn-primary');
-
-let progress = 0;
-
-nextPageBtn.addEventListener('click', () => {
-  // Adiciona 25% ao progresso
-  progress += 25;
-
-  // Atualiza a largura da barra de progresso e a porcentagem
-  progressBar.style.width = `${progress}%`;
-  progressBar.setAttribute('aria-valuenow', progress);
-  progressBar.textContent = `${progress}% completo`;
-});
 
 
 // aparecer a seção quando marca SIM para cirurgia
@@ -50,14 +21,14 @@ sim.addEventListener('click', function() {
   qualCirurgia.style.display = 'block';
 });
 
-const esporteSimRadio = document.getElementById("sim");
+const esporteSimRadio = document.getElementById("esporte-sim");
 const esporteQualContainer = document.getElementById("esporte-qual-container");
 
 esporteSimRadio.addEventListener("click", function() {
   esporteQualContainer.style.display = "block";
 });
 
-const esporteNaoRadio = document.getElementById("nao");
+const esporteNaoRadio = document.getElementById("esporte-nao");
 
 esporteNaoRadio.addEventListener("click", function() {
   esporteQualContainer.style.display = "none";
